a67d07d437b83bb1660227d790dceab08bd69e81
andrea1632 <andrea.bellucci10@libero.it>
2022-04-20 15:43:15 +0200

    added bootstrap5 & axios

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import "bootstrap"
+export default {
+  name: 'App',
+  components: {
+
+  },
+    data(){
+    return{
+      cardsArray: []
+    }
+  },
+  created(){
+    axios.get( 'https://flynn.boolean.careers/exercises/api/array/music' )
+    .then ( (res)=>{
+      this.cardsArray = res.data.response
+    } )
+    .catch( (error) => {
+     console.log( error )
+    } )
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css";
+
+
+</style>
