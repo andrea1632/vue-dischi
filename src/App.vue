f9fe85fefc4caa9d5fa09415dcd6aefff1f6c98b
andrea1632 <andrea.bellucci10@libero.it>
2022-04-20 17:17:54 +0200

    added loader

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <TopLogo/>
     <div class="bgSpotify">
       <div class="container cardContainer pb-5">
-        <div class="row p-5">
+        <div class="row p-5" v-if="cardsArray.length == arrayIndexLength">
           <CardComp
           v-for="(elm, index) in cardsArray" :key="index"
           :image="elm.poster"
@@ -11,6 +11,9 @@
           :author="elm.author"
           :year="elm.year"
           />
+        </div>
+        <div v-else class="text-white loader">
+          <img src="./assets/img/giphy.gif" alt="">
         </div>
       </div>
     </div>
@@ -31,13 +34,16 @@ export default {
   },
     data(){
     return{
-      cardsArray: []
+      cardsArray: [],
+      arrayIndexLength: null,
     }
   },
   created(){
     axios.get( 'https://flynn.boolean.careers/exercises/api/array/music' )
     .then ( (res)=>{
       this.cardsArray = res.data.response
+      this.arrayIndexLength = res.data.response.length
+      console.log(this.arrayIndexLength)
     } )
     .catch( (error) => {
      console.log( error )
@@ -50,5 +56,14 @@ export default {
 @import "bootstrap/dist/css/bootstrap.min.css";
 .bgSpotify{
     background-color: #1E2D3B;
+}
+.loader{
+  width: 80%;
+  margin: 0 auto;
+  height: calc(100vh - 80px);
+  img{
+    width: 100%;
+    margin-top: 100px;
+  }
 }
 </style>
